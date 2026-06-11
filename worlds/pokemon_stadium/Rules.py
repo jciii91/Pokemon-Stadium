f660f59959b4f27f988a36e773a0d658e2ad6a46
john costello <jcostello711@gmail.com>
2026-06-10 20:42:41 -0400

    Add rule for full clear victory condition

--- worlds/pokemon_stadium/Rules.py
+++ worlds/pokemon_stadium/Rules.py
@@ -58,12 +58,17 @@ def set_rules(world: "PokemonStadiumWorld"):
 
     # Beat Rival Rule
     badges = ["Boulder Badge", "Cascade Badge", "Thunder Badge", "Rainbow Badge", "Soul Badge", "Marsh Badge", "Volcano Badge", "Earth Badge"]
-    set_rule(world.multiworld.get_location("Beat Rival", player), lambda state: state.has_all(badges, player))
+    has_all_badges = lambda state: state.has_all(badges, player)
+    set_rule(world.multiworld.get_location("Beat Rival", player), has_all_badges)
 
     # Master Ball Cups Cleared Rule
     collected_all_poke_cup_tiers = lambda state: state.count('Poké Cup - Tier Upgrade', player) > 2
     collected_all_prime_cup_tiers = lambda state: state.count('Prime Cup - Tier Upgrade', player) > 2
     set_rule(world.multiworld.get_location("Master Ball Cups Cleared", player), collected_all_poke_cup_tiers and collected_all_prime_cup_tiers)
+
+    # Rival + Cups Rule
+    full_clear = has_all_badges and collected_all_poke_cup_tiers and collected_all_prime_cup_tiers
+    set_rule(world.multiworld.get_location('Beat Rival and Clear Both Master Ball Cups', player), full_clear)
 
     # Victory condition rule!
     world.multiworld.completion_condition[player] = lambda state: state.has("Victory", player)
